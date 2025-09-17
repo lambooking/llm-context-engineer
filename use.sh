15c59ffebac7fca8a6272d507a180845817c87a6
lambooking <2018566060@qq.com>
2025-09-17 10:49:07 +0800

    [update]:更新使用脚本，注意输出目录

--- use.sh
+++ use.sh
@@ -1,1 +1,1 @@
- python batch_processor.py /workspace/input_path  ~/workspace/output/result-7b-6 --config config/config.yaml --log-level DEBUG
+ python batch_processor.py /workspace/input_path  /workspace/output/result-7b-6 --config config/config.yaml --log-level DEBUG
